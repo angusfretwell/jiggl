029486bd138e09f5716171e1ae8cf1ad1b38a514
Angus Fretwell <angusfretwell@me.com>
2016-05-24 16:38:28 +1000

    feat: add sync worklog job

--- worker.js
+++ worker.js
@@ -1,5 +1,6 @@
 require('./bootstrap');
 const queue = require('./lib/queue');
 
+queue.process('sync worklog', require('./lib/jobs/syncWorklog'));
 queue.process('upsert toggl task', require('./lib/jobs/upsertTogglTask'));
 queue.process('archive toggl task', require('./lib/jobs/archiveTogglTask'));
