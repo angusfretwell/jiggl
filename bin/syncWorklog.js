02c0df7764bc91e3cbf8e67ed36801f1a621a0e6
Angus Fretwell <angusfretwell@me.com>
2016-05-24 16:40:06 +1000

    refactor: rename sync worklog to sync time entries

--- bin/syncWorklog.js
+++ bin/syncWorklog.js
@@ -3,6 +3,6 @@
 require('../bootstrap');
 const queue = require('../lib/queue');
 
-queue.create('sync worklog').priority('critical').save((err) => {
+queue.create('sync time entries').priority('critical').save((err) => {
   process.exit(!!err);
 });
--- worker.js
+++ worker.js
@@ -1,7 +1,7 @@
 require('./bootstrap');
 const queue = require('./lib/queue');
 
-queue.process('sync worklog', require('./lib/jobs/syncWorklog'));
+queue.process('sync time entries', require('./lib/jobs/syncTimeEntries'));
 queue.process('sync time entry', require('./lib/jobs/syncTimeEntry'));
 queue.process('upsert toggl task', require('./lib/jobs/upsertTogglTask'));
 queue.process('archive toggl task', require('./lib/jobs/archiveTogglTask'));
