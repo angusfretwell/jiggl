a5b011e5eac285aa1f3f3007c1f0fe8d1f8bd12d
Angus Fretwell <angusfretwell@me.com>
2016-05-24 16:59:41 +1000

    Worklog sync (#26)
    * refactor: seperate job processing into worker process
    
    * feat: add module exports babel plugin
    
    * refactor: make knexfile work without babel-node
    
    * refactor: reduce priority of archive toggl task job
    
    * refactor: use npm scripts in procfile
    
    * refactor: add npm scripts for worker and worklog sync
    
    * feat: add script to queue worklog sync job
    
    * refactor: increase priority and add calback for sync script
    
    * chore: add moment dependency
    
    * feat: add worklog create and update helper functions
    
    * feat: add sync worklog job
    
    * feat: add sync time entry job
    
    * refactor: rename sync worklog to sync time entries

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 require('./bootstrap');
 require('./lib/app');
+
+// Run the worker in the web process in debug mode
+if (process.env.APP_DEBUG) {
+  require('./worker');
+}
--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,4 @@
-import './bootstrap';
+require('./bootstrap');
 
 module.exports = {
   client: 'pg',
