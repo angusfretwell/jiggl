b67e4cf77ec541795aef2bc56a33f9954e5a50d6
Angus Fretwell <angusfretwell@me.com>
2016-05-24 16:38:53 +1000

    feat: add sync time entry job

--- worker.js
+++ worker.js
@@ -2,5 +2,6 @@ require('./bootstrap');
 const queue = require('./lib/queue');
 
 queue.process('sync worklog', require('./lib/jobs/syncWorklog'));
+queue.process('sync time entry', require('./lib/jobs/syncTimeEntry'));
 queue.process('upsert toggl task', require('./lib/jobs/upsertTogglTask'));
 queue.process('archive toggl task', require('./lib/jobs/archiveTogglTask'));
