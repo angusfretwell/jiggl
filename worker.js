0bde9cf283fd2043f5dfaed1d71cc677d3d5d03a
Angus Fretwell <angusfretwell@me.com>
2016-05-26 22:41:58 +1000

    refactor: move all entry points to bin

--- worker.js
+++
@@ -1,7 +0,0 @@
-require('./bootstrap');
-const queue = require('./lib/queue');
-
-queue.process('sync time entries', require('./lib/jobs/syncTimeEntries'));
-queue.process('sync time entry', require('./lib/jobs/syncTimeEntry'));
-queue.process('upsert toggl task', require('./lib/jobs/upsertTogglTask'));
-queue.process('archive toggl task', require('./lib/jobs/archiveTogglTask'));
